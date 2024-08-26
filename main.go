6b78ad77593c74706567ebbb454b6e9dfbf7e733
Defindd <prilukov_nikita@mail.ru>
2024-08-26 19:33:30 +0400

    interface implementation

--- main.go
+++ main.go
@@ -12,16 +12,24 @@ func (x X) outP() {
 	fmt.Println(x.V)
 }
 func main() {
-	var i interface{} = "hello"
 
-	s := i.(string)
-	fmt.Println(s)
+	var f AB = Foo{}
+	g, ok := f.(BC)
+	_ = g
+	fmt.Println(ok)
+}
 
-	s, ok := i.(string)
-	fmt.Println(s, ok)
+type Foo struct{}
 
-	f, ok := i.([]rune)
-	fmt.Println(f, ok)
-	g := i.(float64) // panic
-	fmt.Println(g)
+func (f Foo) A() {}
+func (f Foo) B() {}
+func (f Foo) C() {}
+
+type AB interface {
+	A()
+	B()
+}
+type BC interface {
+	B()
+	C()
 }
