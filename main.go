534872950a7ae1f029c26272b98392592adffac5
Defindd <prilukov_nikita@mail.ru>
2024-08-26 19:04:18 +0400

    type assertion

--- main.go
+++ main.go
@@ -4,12 +4,24 @@ import (
 	"fmt"
 )
 
+type X struct {
+	V int
+}
+
+func (x X) outP() {
+	fmt.Println(x.V)
+}
 func main() {
-	dfas := "qwerty"
-	runeDfas := []rune(dfas)
-	for i := 0; i < len(runeDfas); i++ {
-		fmt.Println('a', 'A')
-		runeDfas[i] += 'A' - 'a'
-	}
-	fmt.Println(string(runeDfas))
+	var i interface{} = "hello"
+
+	s := i.(string)
+	fmt.Println(s)
+
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	f, ok := i.([]rune)
+	fmt.Println(f, ok)
+	g := i.(float64) // panic
+	fmt.Println(g)
 }
